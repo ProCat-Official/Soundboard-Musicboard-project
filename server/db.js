04a8ede60ac9fc789494a6a95940d6ebc4cee6f4
𝙿𝚛𝚘 𝙲λ𝚝 <klasniykot41@gmail.com>
2026-06-27 23:22:55 +0300

    db.js

--- server/db.js
+++ server/db.js
@@ -1,39 +1,27 @@
 const mysql = require('mysql2/promise');
-const env = require('dotenv').config().parsed;
 
 const db = mysql.createPool({
-    host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || '',
-    database: env.DB_NAME || 'music_db',
-    port: env.DB_PORT || 3306,
+    host: 'b0zk9jti9thx6un2mqhf-mysql.services.clever-cloud.com',
+    user: 'uv9lylzu36pyxp0v',
+    password: 'ghLetKJZBFR7smboyInd',
+    database: 'b0zk9jti9thx6un2mqhf',
+    port: 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
     connectTimeout: 30000,
-    acquireTimeout: 30000,
-    timeout: 30000,
     ssl: {
-        rejectUnauthorized: false,
-
-        minVersion: 'TLSv1.2'
-    },
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0
+        rejectUnauthorized: false
+    }
 });
 
-// тест 
 (async function testConnection() {
     try {
         const connection = await db.getConnection();
-        console.log('database connected');
+        console.log('Clever Cloud connected');
         connection.release();
     } catch (error) {
-        console.error('Failed to connect database', error.message);
-        console.error('Check yuor values:');
-        console.error('DB_HOST:', process.env.DB_HOST || env.DB_HOST);
-        console.error('DB_USER:', process.env.DB_USER || env.DB_USER);
-        console.error('DB_NAME:', process.env.DB_NAME || env.DB_NAME);
+        console.error('Failed to connect Database', error.message);
     }
 })();
 
